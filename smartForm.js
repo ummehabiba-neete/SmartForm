fb63059bd3512693577fbc2f2934acb0fc2c9c93
cis101 <cis101@secs.iub.edu.bd>
2017-10-11 08:03:45 +0600

    SmartForm

--- smartForm.js
+++ smartForm.js
@@ -1,3 +1,53 @@
 // Your jQuery goes here
 
-var userData = {};
+var userData = {
+name:"",
+email:"",
+html:{likes:[],dislikes:[]},
+css:{likes:[],dislikes:[]},
+js:{likes:[],dislikes:[]},
+strength:{html:"",css:"",js:""},
+currentQuestion:"welcome"};
+
+$("#startBtn").click(function(event)
+{
+
+$("#welcome"). hide();
+$("#q1").show;
+});
+
+$("#name").change(function(event)){
+
+	console.log($("# name").val());
+});
+$("#email").change(function(event)){
+	console.log($("# email").val());
+	if ($("#email").val()!="" && $("#name").val())
+		$("#q1Next").prop ("disabled",false);}
+});
+$("#q1Next").click(function(event))
+{userData.name=$("#name").val();
+userData.email=$("#email").val();
+$("#q1").hide();
+$("#q2").show();
+});
+$("#htmlQ").click(function(event){
+
+$("#q2").hide();
+$("#q2b").show();
+
+});
+$("#jsQ").click(function(event){
+
+$("#q2").hide();
+$("#q2c").show();
+
+});
+
+
+$(".prevQ2") .click(function(event)
+{
+$("#q2") .show();
+$(this).parent().parent().parent()hide();
+
+});
